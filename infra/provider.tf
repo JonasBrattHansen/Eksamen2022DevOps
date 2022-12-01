159bb34c1e9f85bdc8728c0d4ed3fa546ae38dc9
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-01 13:51:56 +0100

    Changed provider.tf file

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,13 +7,12 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "1029_bucket" {
+resource "aws_s3_bucket" "1029_mybucket" {
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.1029_bucket.bucket
-
+  bucket = aws_s3_bucket.1029_mybucket.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
