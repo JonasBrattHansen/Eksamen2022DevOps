5541d33f2a15c1f9a299a4ac5a880ebb34ecb044
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-01 13:47:12 +0100

    Changed provider.tf file

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,20 +6,4 @@ variable "candidate_email"  {
   type = string
 }
 
-variable "carts"  {
-  type = string
-}
-
-variable "cartsvalue"  {
-  type = string
-}
-
-variable "checkouts"  {
-  type = string
-}
-
-variable "checkout_latency"  {
-  type = string
-}
-
 
