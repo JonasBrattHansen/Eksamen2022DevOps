c2d00ae22cf0aaf514b3e632100508b2063979d1
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-01 13:42:42 +0100

    Changed provider.tf file

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,10 +5,4 @@ terraform {
       version = "4.40.0"
     }
   }
-
-  backend "s3" {
-      bucket = "pgr301-2021-terraform-state"
-      key    = "glenn.richard.bech/apprunner-a-new-state.state"
-      region = "eu-north-1"
-    }
 }
