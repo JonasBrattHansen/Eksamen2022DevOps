453b3d7e53cb3c0e8f8c256582d2c4139d2183e0
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-01 14:57:49 +0100

    changed docker yml file

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "pgr301-2021-terraform-state"
+    bucket = "1029_terraform_bucket"
     key    = "glenn.richard.bech/apprunner-a-new-state.state"
     region = "eu-north-1"
   }
