b34f7b365f7a464f668f3f4abe383f3b6225ce5c
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-01 13:50:49 +0100

    Changed provider.tf file

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,6 +13,7 @@ resource "aws_s3_bucket" "1029_bucket" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.1029_bucket.bucket
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
