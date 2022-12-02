3861e1d94eaed6eb0b20eca6cdc35c531a4464fa
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-02 13:18:36 +0100

    changes

--- infra/provider.tf
+++ infra/provider.tf
@@ -12,14 +12,6 @@ terraform {
       key    = "analytics-1029.state"
       region = "eu-west-1"
   }
-    resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-      bucket = aws_s3_bucket.analytics-1029.bucket
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm     = "AES256"
-        }
-      }
-    }
 
 }
 
