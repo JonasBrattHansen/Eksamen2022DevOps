3e126f5c8bda99159ea8e36a2484afe1b8ee5e6c
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-02 15:43:14 +0100

    changes

---
+++ infra/alarm.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "carts-should-be-under-value-5"
+  namespace                 = "joha062"
+  metric_name               = "carts_count.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off if number of carts over three repetedly periods on 5 minutes exceeds 5 "
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = var.student_name
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = "joha062@student.kristiania.no"
+}
