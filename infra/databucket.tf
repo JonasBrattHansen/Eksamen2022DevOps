1309ae558432d7f3fd779364bcc2310f0ae27111
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-02 13:20:03 +0100

    changes

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,12 +1,4 @@
 resource "aws_s3_bucket" "analyticsbucket" {
   bucket = "analytics-${var.candidate_id}"
 }
-    resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-      bucket = aws_s3_bucket.analytics-1029.bucket
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm     = "AES256"
-        }
-      }
-    }
 
