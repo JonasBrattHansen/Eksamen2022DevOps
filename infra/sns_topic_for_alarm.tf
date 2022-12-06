fd0c23001f18b4637f2998fa13ecb4668a561af0
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-06 21:07:45 +0100

    READme update

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -5,7 +5,7 @@ resource "aws_sns_topic" "alarms" {
 resource "aws_cloudwatch_metric_alarm" "zerosum" {
   alarm_name                = "carts-over-5-1029"
   namespace                 = "joha062"
-  metric_name               = "carts_count.value"
+  metric_name               = "carts_counter.value"
 
   comparison_operator       = "GreaterThanThreshold"
   threshold                 = "5"
