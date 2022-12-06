9d67d69406c17cd13f479cb31e9f950f200f0b3c
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-06 22:40:23 +0100

    update in metrics

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -16,9 +16,12 @@ resource "aws_cloudwatch_metric_alarm" "zerosum" {
 
   alarm_description         = "This alarm goes off if number of carts over three repeatedly periods on 5 minutes exceeds 5 "
   insufficient_data_actions = []
-  alarm_actions       = [aws_sns_topic.alarms.arn]
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
 }
 
+resource "aws_sns_topic" "user_updates" {
+  name = alarm-topic-${var.candidate_id}
+}
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.user_updates.arn
