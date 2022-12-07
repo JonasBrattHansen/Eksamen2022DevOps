9f2ac76e0d8cf0079d0f50a8b7039ccc8f81d824
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-07 16:10:19 +0100

    update in metrics

--- infra/alarm.tf
+++ infra/alarm.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_metric_alarm" "zerosum" {
   comparison_operator       = "GreaterThanThreshold"
   threshold                 = "5"
   evaluation_periods        = "3"
-  period                    = "10"
+  period                    = "60"
 
   statistic                 = "Maximum"
 
