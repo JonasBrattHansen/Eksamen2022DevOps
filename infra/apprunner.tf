98f31aca5433daf65bb9a1ebdfdf716090b5f946
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-09 13:06:07 +0100

    update in metrics

--- infra/apprunner.tf
+++ infra/apprunner.tf
@@ -5,6 +5,14 @@ resource "aws_apprunner_service" "service" {
     authentication_configuration {
       access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
     }
+    image_repository {
+          image_configuration {
+            port = "8080"
+          }
+          image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/1029:latest"
+          image_repository_type = "ECR"
+        }
+
     auto_deployments_enabled = false
   }
 }
