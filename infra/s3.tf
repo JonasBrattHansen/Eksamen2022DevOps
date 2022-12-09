943a0024d34122b09823699bc7e3eccfed9cb7fe
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-09 13:19:23 +0100

    update in metrics

---
+++ infra/s3.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "analyticsbucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example4exam" {
+  bucket = aws_s3_bucket.analyticsbucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+variable "bucket_name" {
+  type = string
+  default = "analytics-1029"
+}
