43c23833db8d47616306306a41a1dd0122d50e8a
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-09 11:57:07 +0100

    update in metrics

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,4 +1,9 @@
 resource "aws_s3_bucket" "analyticsbucket" {
   bucket = "analytics-${var.candidate_id}"
+  {
+  "Effect" : "Allow",
+  "Resource": "arn:aws:s3:::${var.candidate_id}/*",
+  "Principal": "*"
+  }
 }
 
