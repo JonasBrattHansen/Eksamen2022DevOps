b6f79e57a60db63a313df4ad26473e6d6ea479e0
Jonas Bratt Hansen <69790914+JonasBrattHansen@users.noreply.github.com>
2022-12-09 13:20:43 +0100

    update in metrics

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.33.0"
-    }
-  }
-}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example4exam" {
   bucket = aws_s3_bucket.analyticsbucket.bucket
